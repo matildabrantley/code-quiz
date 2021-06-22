84902ef3153a62f59fc3952637a5e591d7b15467
Matilda Brantley <matildahbrantley@gmail.com>
2021-06-22 18:32:42 -0400

    styled, debugged, commented, added screenshots

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # Homework 4: Code Quiz
+#### By Matilda Brantley 
 
+[Live GitHub Page](https://matildabrantley.github.io/homework3-password-generator/)
 ## Features
 * Class **Question** builds multiple choice question objects with correct answer. 
     * Method displayQuestion changes main-text (h1) to this question text, 
     shows multiple choice buttons and adds event listeners to those buttons.
     * Method answerQuestion is passed to button click events.
-* Functions startQuiz and endQuiz 
-    * Styles updated properly
-    * startQuiz starts timer, endQuiz stops it
+* Functions startQuiz and endQuiz handle timer events and setting up or resetting the quiz.
+* Functions for handling localStorage and displaying high scores include:
+    * submitHighScore - Form submission for name/score
+    * addHighScore - sorts and adds to localStorage array of pairs
+    * getHighScore - returns localStorage pair array (2D array)
+    * updateHighScores - removes and reappends list in order
+    * clearHighScores - empties localStorage and removes list items
+    * viewHighScores - displays high scores and toggles event listeners and text for view-scores
+    * hideHighScores - hides scores, reverts text and event listeners for view-scores
+* Function originalStyles targets every element in the DOM tree and sets their styles to null, reverting them to default in css.style. Simplifies resetting quiz and hiding elements (instead of appending/removing). 
+* Styling for high score list: alternating colors, best score is larger, hover lights up and enlarges score.
+* Ten questions so score only goes down 5 on wrong answers and 0.5 per second.
+* Creeping blue radial-gradient to add suspense as score ticks down.
+
+## Screenshots
+
+![Screenshot One](assets/images/quiz1.jpg)
+![Screenshot Two](assets/images/quiz2.jpg)
+![Screenshot Three](assets/images/quiz3.jpg)
+![Screenshot Four](assets/images/quiz4.jpg)
+![Screenshot Five](assets/images/quiz5.jpg)
+![Screenshot Six](assets/images/quiz6.jpg)
 
 ## User Story
 
@@ -29,4 +50,3 @@ Given I am taking a code quiz
     * Then the game is over
 * When the game is over
     * Then I can save my initials and my score
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
+//target elements
 var main = document.getElementsByTagName("main")[0]; //main section where quiz occurs
 var mainText = document.getElementById("main-text"); //titles, questions
 var questionBox = document.getElementById("question-box");
 var startButton = document.getElementById("start");
-var viewScoresElement = document.getElementById("view-scores");
+var toggleViewScores = document.getElementById("view-scores");
+var scoresList = document.getElementById("ordered-scores-list");
 var scoreText = document.getElementById("score");
-var questions = [];
-var currentQuestion;
-var score;
-var scoreTimer;
+var submitButton= document.getElementById("submit");
+var backButton = document.getElementById("back-button");
+var clearButton = document.getElementById("clear-button");
 
+//create globals
+var questions = []; //Question object array
+var currentQuestion; //index of questions array
+var score; //test-taker's score
+var scoreTimer; //score's setInterval
+
+//add event listeners
 startButton.addEventListener("click", startQuiz);
-viewScoresElement.addEventListener("click", viewHighScores);
+toggleViewScores.addEventListener("click", viewHighScores);
+submitButton.addEventListener("click", submitHighScores);
+backButton.addEventListener("click", goBack);
+clearButton.addEventListener("click", clearHighScores);
 
-//multiple choice question
+//multiple choice Question class
 class Question {
     constructor(questionText, choicesText, correct){
         this.questionText = questionText; //string
@@ -28,7 +39,7 @@ class Question {
         if (choiceButton.value == this.correct){
             document.getElementById("response").textContent = "Correct!";
         } else {
-            score -= 10;
+            score -= 5;
             document.getElementById("response").textContent = "Wrong!";
         }
 
@@ -49,6 +60,7 @@ class Question {
 
     }
 
+    //set question text and add choices
     displayQuestion() {
         //set h1 to question text
         mainText.textContent = this.questionText;
@@ -79,39 +91,39 @@ class Question {
     }
 }
 
-
+//start quiz with timer, reset questions and score
 function startQuiz() {
-    console.log("startQuiz");
     main.style.left = "30%";
     mainText.style.fontSize = "2vw";
     mainText.style.textAlign = "left";
     document.getElementById("annoucement").textContent = "";
     startButton.style.display = "none"; //simply hide start button
-    console.log("starting");
     
     //start score countdown timer
     scoreTimer = setInterval(function(){
         //creeping redness from sides as score goes down for suspense!
-        document.getElementById("body").style.backgroundImage = "radial-gradient(circle, white, white, red " + (100+score) + "%)";
+        document.getElementById("body").style.backgroundImage = "radial-gradient(circle, white, white, blue " + (100+score) + "%)";
         //if (score < 25)
             //document.getElementById("body").style.backgroundImage = "radial-gradient(circle, white, white, red " + (100+score) + "%)";
-        score -= 0.1;
+        score -= 0.05;
         scoreText.textContent = round(score);
         if (score <= 0)
             endQuiz();
     }, 100);
 
+    //setup quiz
     createQuestions();
     score = 100;
     currentQuestion = 0;
+    //display first question
     questions[currentQuestion].displayQuestion();
 }
 
-
+//clear timer and lingering questions/choice buttons, add final buttons
 function endQuiz() {
-    console.log("endQuiz");
     clearInterval(scoreTimer);
     mainText.textContent = "All Done!";
+    mainText.style.marginLeft = "20%";
     main.style.left = "40%";
 
     //clear lingering buttons when game ends at zero
@@ -124,84 +136,94 @@ function endQuiz() {
     document.getElementById("annoucement").textContent = "Your final score is " + round(score)  + ".";
 
     document.getElementById("initials-form").style.display = "block";
-    var submitButton= document.getElementById("submit");
-    submitButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        var initials = document.querySelector("#initials").value;
-        document.getElementById("initials-form").style.display = "none";
-        updateHighScores([initials, round(score)]);
-        finalMenu();
-    });
 }
 
-function finalMenu(){
-    console.log("finalMenu");
-    var backButton = document.createElement("button");
-    backButton.textContent = "Go Back";
-    backButton.addEventListener("click", function() {
-
-        mainText.textContent = "Coding Quiz Challenge";
-        document.getElementById("annoucement").textContent = "Try to answer the following code-related questions within" + 
-        "the time limit. Keep in mind that incorrect answers will penalize your time score by ten seconds.";
-        main.removeChild(this);
-
-        originalStyles(); //bring back original css styles 
-    });
-    main.appendChild(backButton);
-
-    //Add clear high scores button
-    var clearButton = document.createElement("button");
-    clearButton.textContent = "Clear High Scores";
-    clearButton.addEventListener("click", function() {
-        localStorage.clear();
-        main.removeChild(this);
-    });
-    main.appendChild(clearButton);
+function submitHighScores(event) {
+    event.preventDefault();
+    var initials = document.querySelector("#initials").value;
+    document.getElementById("initials-form").style.display = "none";
+    addHighScore(initials, round(score));
+    backButton.style.display = "inline";
+    clearButton.style.display = "inline";
 }
 
-function updateHighScores(initials){
-    console.log("updateHighScores");
-    //if first person to score
-    if (localStorage.getItem("highScores") === null)
-        localStorage.setItem("highScores", JSON.stringify(new Array())); //stringify empty array... *head scratch*
-    //create local array to store highScore array of initials from localStorage
-    var highscoreArray = JSON.parse(localStorage.getItem("highScores"));
+function goBack() {
+    mainText.textContent = "Coding Quiz Challenge";
+    document.getElementById("annoucement").textContent = "Try to answer the following code-related questions within" + 
+    "the time limit. Keep in mind that incorrect answers will penalize your time score by ten seconds.";
+    originalStyles(); //bring back original css styles 
+}
+
+function clearHighScores() {
+    localStorage.clear();
+    scoresList.style.display = "none";
+    clearButton.style.display = "none";
+
+    //clear list
+    while (scoresList.firstChild)
+        scoresList.removeChild(scoresList.firstChild);
+}
+
+function addHighScore(initials, newScore){
+    var highScoresArray = getHighScores(); //get high scores from localStorage
     //push new item to local array
-    highscoreArray.push(initials);
+    highScoresArray.push([initials, newScore]);
 
-    //sort high scores based on 
-    highscoreArray = highscoreArray.sort(function(score1, score2) {
+    //resort high scores based on this function
+    highScoresArray = highScoresArray.sort(function(score1, score2) {
         return score2[1] - score1[1];
     });
 
     //set localStorage to updated array
-    localStorage.setItem("highScores", JSON.stringify(highscoreArray));
+    localStorage.setItem("highScores", JSON.stringify(highScoresArray));
+
+    //update list after sort
+    updateHighScoresList(highScoresArray);
 }
 
- function getHighScores(){
-     return JSON.parse(localStorage.getItem("highScores"));
- }
-
- function viewHighScores(){
-    console.log("viewHighScores");
-    var highScoresArray = getHighScores(); //retrieve high scores from localStorage
-    var highScoresSection = document.getElementById("high-scores");
-    document.getElementById("view-scores").textContent = "Hide High Scores";
-    var scoresList = document.createElement("ol");
-    highScoresSection.append(scoresList);
-    for (var i = 0; i < highScoresArray.length; i++){
+//update high score ordered list with sorted scores
+function updateHighScoresList(sortedScores){
+    //clear list
+    while (scoresList.firstChild)
+        scoresList.removeChild(scoresList.firstChild);
+    //re-append list items in order
+    for (var i = 0; i < sortedScores.length; i++){
         var listItem = document.createElement("li");
-        listItem.textContent = highScoresArray[i][1];
-        scoresList.append(listItem);
-    }  
+        listItem.textContent = sortedScores[i][0] + ": " + sortedScores[i][1];
+        scoresList.append(listItem);  
+    }
+}
 
-    document.getElementById("view-scores").textContent = "Hide High Scores";
-    viewScoresElement.removeEventListener("click", viewHighScores);
-    viewScoresElement.addEventListener("click", function(){
-        scoresList.remove();
-        viewScoresElement.addEventListener("click", viewHighScores);
-        document.getElementById("view-scores").textContent = "Show High Scores";
-    });
+//create local array to store highScore array of initials from localStorage
+function getHighScores() {
+    //if first person to score
+    if (localStorage.getItem("highScores") === null)
+         localStorage.setItem("highScores", JSON.stringify(new Array())); //stringify empty array... *head scratch*
+     return JSON.parse(localStorage.getItem("highScores"));
+}
+
+//Toggle high scores on
+ function viewHighScores(){
+    updateHighScoresList(getHighScores());
+
+     //unhide list
+    scoresList.style.display = "block";
+    //toggle text to "Hide"
+    toggleViewScores.textContent = "Hide High Scores";
+
+    //remove and add event listeners
+    toggleViewScores.removeEventListener("click", viewHighScores);
+    toggleViewScores.addEventListener("click", hideHighScores);
+}
+
+//Toggle high scores off
+function hideHighScores(){
+    //toggle text back to "Show"
+    toggleViewScores.textContent = "Show High Scores";
+    scoresList.style.display = "none";
+    //remove and add event listeners
+    toggleViewScores.removeEventListener("click", hideHighScores);
+    toggleViewScores.addEventListener("click", viewHighScores);
 }
 
 //round to one decimal place (for really close scores)
@@ -209,7 +231,6 @@ function round(value) {return Math.round(value * 10) / 10;}
 
 //Reverts every element back to its original style! 
 function originalStyles() {
-    console.log("originalStyles");
     //select every element in the DOM
     var elements = document.getElementsByTagName("*");
     for (var i = 0; i < elements.length; i++) {
@@ -230,14 +251,16 @@ function originalStyles() {
     }
 }
 
+//build array of Question objects
 function createQuestions () {
     questions.push(new Question("The HTML <h1> tag is the _____ size heading", ["Smallest", "Medium", "Largest",], 2));
-    questions.push(new Question("What JavaScript function returns an HTML element by its ID?", ["getId()", "findElementById()", "importElement()", "getElementById()"], 3));
+    questions.push(new Question("Bootstrap is a framework for which language?", ["JavaScript", "C++", "CSS", "PHP"], 2));
     questions.push(new Question("Where is a webpage's localStorage held?", ["Client-side", "Server-side", "Both", "Neither"], 0));
+    questions.push(new Question("Which data type is used to switch values between only two states?", ["State", "Boolean", "Object", "On-Off"], 1));
+    questions.push(new Question("What JavaScript function returns an HTML element by its ID?", ["getId()", "findElementById()", "importElement()", "getElementById()"], 3));
     questions.push(new Question("What JavaScript syntax separates multiple name:value pairs within objects?", [",", ";", "!", "=", "#", "$"], 0));
-    questions.push(new Question("Bootstrap is a framework for which language?", ["JavaScript", "Java", "CSS", "PHP"], 2));
-    questions.push(new Question("Which data type is used to switch values between only two states?", ["On-Off", "Boolean", "Object", "String"], 1));
     questions.push(new Question("Which JavaScript keyword refers to the (closest) object it belongs to?", ["Let", "Home", "Here", "This"], 3));
     questions.push(new Question("Which JavaScript keyword makes a template for creating objects (provided a constructor)?", ["function", "template", "construct", "class"], 3));
-    questions.push(new Question("True or False: As analogy, if HTML is the noun and CSS the adjective/adverb, JavaScript is most like the verb.", ["True", "False"], 0));
+    questions.push(new Question("True or False: JavaScript is an extension of Java", ["True", "False"], 1));
+    questions.push(new Question("True or False: As analogy, if HTML is the noun and CSS the adjective, JavaScript is most like the verb.", ["True", "False"], 0));
 }
